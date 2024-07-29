dcef5bc427adee2c6e18759c3091fa59f393002a
Jorge Sáez Garrido <jsaegar@protonmail.com>
2024-07-29 19:32:31 +0200

    Entire code restructuring

---
+++ game.h
@@ -0,0 +1,24 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <string>
+
+enum class box {
+    X,
+    O,
+    VOID
+};
+enum direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+/// @brief 
+/// @param player1 
+/// @param player2 
+/// @return -1 if draw, 0 if player1 wins, 1 if player2 wins
+int startGame(const std::string &player1, const std::string &player2);
+
+#endif
