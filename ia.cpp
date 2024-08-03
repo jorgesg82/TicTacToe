2f5b640cd06e4e9064c8fb823e74f0067e558ae1
Jorge Sáez Garrido <jsaegar@protonmail.com>
2024-08-03 20:24:10 +0200

    Implemented MINIMAX algorithm

--- ia.cpp
+++ ia.cpp
@@ -1,86 +1,117 @@
 #include "ia.h"
 
-bool isValid(Pair pair) {
-    if (pair.first < 0 || pair.first > 2
-    || pair.second < 0 || pair.second > 2) {
+Node::Node(const Board &gameStatus, const bool &max)
+{
+    this->gameStatus = gameStatus;
+    this->max = max;
+}
+
+bool isValid(Pair pair)
+{
+    if (pair.first < 0 || pair.first > 2 || pair.second < 0 || pair.second > 2)
+    {
         return false;
     }
     return true;
 }
 
-bool Node::equals(const Pair& pair1, const Pair& pair2) const {
-    if (this->gameStatus[pair1.first][pair1.second] == this->gameStatus[pair2.first][pair2.second]) {
+bool Node::equals(const Pair &pair1, const Pair &pair2) const
+{
+    if (gameStatus.getValue(pair1) == gameStatus.getValue(pair2))
+    {
         return true;
     }
     return false;
 }
 
-int Node::evaluate() const {
-
+int Node::evaluate() const
+{
+    // Check if win
+    int end = gameStatus.isWin();
+    if (end != -1)
+    {
+        switch (end)
+        {
+        case 0:
+            return 0; // Draw
+            break;
+        case 1:
+            return -1; // Player wins
+            break;
+        case 2:
+            return 1; // IA wins
+            break;
+        }
+    }
     // Branch node
     // Create and evaluate childs
-    bool isChild = true;
     int bestOption;
-    int rowIndex = 0;
-    for (const auto& row : this->gameStatus) {
-        int colIndex = 0;
-        for (const box& b : row) {
-            if (b == box::VOID) {
-                isChild = false;
-                if (this->max) bestOption = -1;
-                else bestOption = 1;
+    if (this->max)
+        bestOption = -2;
+    else
+        bestOption = 2;
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            box b = gameStatus.getValue(std::make_pair(i, j));
+            if (b == box::VOID)
+            {
                 Board newStatus = gameStatus;
-                if (this->max) newStatus[rowIndex][colIndex] = box::X;
-                else newStatus[rowIndex][colIndex] = box::O;
+                if (this->max)
+                {
+                    newStatus.setValue(std::make_pair(i, j), box::O);
+                }
+                else
+                {
+                    newStatus.setValue(std::make_pair(i, j), box::X);
+                }
+
                 Node child = Node(newStatus, !this->max);
                 int value = child.evaluate();
-                if (this->max) {
-                    if (value == 1) return 1;
-                    if (value > bestOption) bestOption = value;
-                } else {
-                    if (value == -1) return -1;
-                    if (value < bestOption) bestOption = value;
+
+                if (this->max)
+                {
+                    if (value > bestOption)
+                        bestOption = value;
+                    //if (value == 1)
+                        //return 1;
+                }
+                else
+                {
+                    if (value < bestOption)
+                        bestOption = value;
+                    //if (value == -1)
+                        //return -1;
                 }
             }
-            ++colIndex;
         }
-        ++rowIndex;
     }
-    if (!isChild) return bestOption;
+    return bestOption;
+}
 
-    // Leaf node
-    bool win = false;
-    bool winner;
-
-    for (int a = 0; a < 3; ++a) {
-        for (int b = 0; b < 3; ++b) {
-            // Go through all the boxes
-            Pair currentPosition = std::make_pair(a,b);
-            for (int i = -1; i < 2; ++i) {
-                for (int j = -1; j < 2; ++j) {
-                    // Go through all adjacent boxes
-                    if (i == 0 && j == 0) continue; // Avoid current box 
-                    Pair secondPosition = std::make_pair(a+i, b+j);
-                    if (!isValid(secondPosition)) continue; // Out of bounds
-                    if (equals(currentPosition, secondPosition)) {
-                        // Second position is valid. Check third position
-                        std::array<Pair, 2> positionsToCheck = {
-                            std::make_pair(a+2*i,b+2*j),
-                            std::make_pair(a-i,b-j)
-                        };
-                        for (const Pair &thirdPosition : positionsToCheck) {
-                            if (isValid(thirdPosition) && equals(currentPosition, thirdPosition)) {
-                                win = true;
-                                box value = gameStatus[currentPosition.first][currentPosition.second];
-                                if(value == box::X) return 1; // Player1 wins
-                                return -1; // Player2 wins
-                                break;
-                            }
-                        }
-                    }
-                }
+Pair IA::play(const Board &board) const
+{
+    Pair choice;
+    Board playBoard;
+    int bestOption = -2;
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            if (board.getValue(std::make_pair(i, j)) != box::VOID)
+                continue;
+            playBoard = board;
+            playBoard.setValue(std::make_pair(i, j), box::O);
+            Pair currentChoice(i, j);
+            Node node = Node(playBoard, false);
+            int value = node.evaluate();
+            if (value > bestOption)
+            {
+                bestOption = value;
+                choice = currentChoice;
             }
-        }   
+        }
     }
-    return 0; // Reached a draw
+    return choice;
 }
