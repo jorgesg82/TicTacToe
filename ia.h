2c1faba1588b024483dccd7c336f2ce191a1ab58
Jorge Sáez Garrido <jsaegar@protonmail.com>
2024-08-03 20:23:46 +0200

    Created IA class to manage nodes

--- ia.h
+++ ia.h
@@ -2,23 +2,29 @@
 #define IA_H
 
 #include "utils.h"
+#include "board.h"
 #include <vector>
 
-class Node {
+class Node
+{
 public:
-    Node(Board gameStatus, bool max) {
-        this->gameStatus = gameStatus;
-        this->max = max;
-    }
-    
-    int evaluate() const;
+    Node(const Board& gameStatus, const bool& max);
+
+    friend class IA;
 
 private:
+    int evaluate() const;
     Board gameStatus;
     bool max;
     std::vector<Node> childs;
 
-    bool equals(const Pair& pair1, const Pair& pair2) const;
+    bool equals(const Pair &pair1, const Pair &pair2) const;
+};
+
+class IA
+{
+public:
+    Pair play(const Board &board) const;
 };
 
 #endif
